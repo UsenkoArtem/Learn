d54b88938e8aa027a3917e6c44b2a050e44368c5
Artem_Usenko <usernko.job@gmail.com>
2016-12-17 12:26:01 +0200

    New changelist

--- src/Table/Controller.java
+++ src/Table/Controller.java
@@ -1,17 +1,39 @@
 package Table;
 
+import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 /**
  * Created by Artem on 16.12.2016.
  */
-public class Controller {
+public class Controller extends Application {
+    @FXML
+    public TextField Text_name;
+    @FXML
+    public TextField Text_name2;
+    @FXML
+    public TextField Text_phone;
+    @FXML
+    public TextField Text_email;
+    @FXML
+    public TextField Text_country;
 
     private ObservableList<Person> usersData = FXCollections.observableArrayList();
 
@@ -19,21 +41,26 @@ public class Controller {
     public TableView<Person> Table;
 
     @FXML
-    public TableColumn<Person , String> Name;
+    public TableColumn<Person, String> Name;
 
     @FXML
-    public  TableColumn<Person , String> LastName;
+    public TableColumn<Person, String> LastName;
 
     @FXML
-    public  TableColumn<Person , String> Phone;
+    public TableColumn<Person, String> Phone;
 
     @FXML
-    public TableColumn<Person , String> email;
+    public TableColumn<Person, String> email;
 
     @FXML
-    public TableColumn<Person , String> Country;
+    public TableColumn<Person, String> Country;
     @FXML
-   private void initialize() {
+    public Button Button_ok;
+    @FXML
+    public Button Button_reset;
+
+    @FXML
+    private void initialize() {
         intitData();
         // устанавливаем тип и значение которое должно хранится в колонке
         Name.setCellValueFactory(new PropertyValueFactory<Person, String>("Name"));
@@ -43,18 +70,51 @@ public class Controller {
         email.setCellValueFactory(new PropertyValueFactory<Person, String>("email"));
 
         Table.setItems(usersData);
-        }
+    }
 
     private void intitData() {
-        usersData.add(new Person("Artem", "Usenko" ,"1416", "gmail.com", "Ukrain"));
+        usersData.add(new Person("Artem", "Usenko", "1416", "gmail.com", "Ukrain"));
     }
 
     public void AddTable(ActionEvent actionEvent) {
+        try {
+        Stage stage = new Stage();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("FXML/User.fxml"));
+            System.out.println("fa");
+            Pane load = (AnchorPane) loader.load();
 
-        Table.setEditable(true);
-        ObservableList<Person> data = Table.getItems();
-        data.add(new Person("Artem", "Usenko" ,"1416", "gmail.com", "Ukrain"));
-        Table.setItems(data);
-        System.out.println("af");
+            stage.setTitle("Редактирование записи");
+
+        stage.setResizable(false);
+        stage.setScene(new Scene(load));
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.initOwner(((Node)actionEvent.getSource()).getScene().getWindow());
+        stage.show();
+
+    } catch (IOException e) {
+            System.out.println("faf");
+    }
+
+    }
+
+    public void Bitton_reset(ActionEvent actionEvent) {
+    }
+
+    public void Bitton_ok(ActionEvent actionEvent) {
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+
+    public void button2(ActionEvent actionEvent) {
+    }
+
+    public void WriteText(ActionEvent actionEvent) {
+    }
+
+    public void button1(ActionEvent actionEvent) {
     }
 }
--- src/Table/Table.java
+++ src/Table/Table.java
@@ -1,33 +1,20 @@
 package Table;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-/**
- * Created by Artem on 16.12.2016.
- */
 public class Table  extends  Application{
-
-        private static Pane root ;
-        private Scene scene;
-
-        public static Pane getRoot() {
-            return root;
-        }
-
-        public static void setRoot(Pane root) {
-            Table.root = root;
-        }
 
         @Override
         public void start(Stage primaryStage) throws Exception{
-
-            setRoot(FXMLLoader.load(getClass().getResource("Table.fxml")));
+            Parent root = FXMLLoader.load(getClass().getResource("Table.fxml"));
             primaryStage.setTitle("Hello World");
-            setScene(new Scene(getRoot()));
-            primaryStage.setScene(getScene());
+            primaryStage.setMinHeight(600);
+            primaryStage.setMinWidth(400);
+            primaryStage.setScene(new Scene(root, 300, 275));
             primaryStage.show();
         }
 
@@ -37,13 +24,6 @@ public class Table  extends  Application{
             launch(args);
         }
 
-        public Scene getScene() {
-            return scene;
-        }
-
-        public void setScene(Scene scene) {
-            this.scene = scene;
-        }
     }
 
 
